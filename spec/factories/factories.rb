6b73c488e94fbb7de3cc0293927b3216c5a1b244
Arsen Shamkhalov <thornu731@gmail.com>
2015-06-12 08:48:35 +0300

    initial user tests

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,3 +1,11 @@
 FactoryGirl.define do
+  factory :user do
+    email
+    password 'password'
+    password_confirmation 'password'
+  end
 
+  sequence(:email) do |n|
+    "user_#{n}@example.com"
+  end
 end
